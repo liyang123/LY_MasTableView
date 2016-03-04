c425c313e3690b07d38a6703ea55473d124e60dd
liyang2016 <ly@liyangdeimac.local>
2016-03-04 18:16:27 +0800

    Initial Commit

---
+++ LY_MasTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LY_MasTableView
+//
+//  Created by ly on 16/3/4.
+//  Copyright © 2016年 liyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
